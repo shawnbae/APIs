57295b1f8d1ea51b7761cb5153ba8c0497dab9fa
shawnbae <tngks0315@naver.com>
2021-04-06 15:21:56 +0900

    20210406 update

--- google-trend.py
+++ google-trend.py
@@ -4,6 +4,24 @@ import os
 
 # 검색 keyword, 검색 기간 입력하기
 keyword = "Galaxy Fold"
-period = "today 3-m"
+period = "today 3-m" # 최근 3개월
 
 # Google Trend 접속하기
+trend_obj = TrendReq()
+trend_obj.build_payload(kw_list = [keyword], timeframe = period)
+
+# 시간에 따른 Trend 변화
+trend_df = trend_obj.interest_over_time()
+print(trend_df.head())
+
+# 그래프 출력
+plt.style.use("ggplot")
+plt.figure(figsize = (14,5))
+trend_df["Galaxy Fold"].plot()
+plt.title("Google Trends over time", size = 15)
+plt.legend(labels = ["Galaxy Fold"], loc = "upper right")
+
+# 그래프 파일 저장하기
+cwd = os.getcwd()
+output_filepath = os.path.join(cwd, "output", "google_trend_%s.png" % keyword)
+plt.savefig(output_filepath, dpi = 300)
