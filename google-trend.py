9a0fa5e893dc58aed5f0b87f147ff98e30208b25
shawnbae <tngks0315@naver.com>
2021-04-06 15:16:40 +0900

    20210406 update

--- google-trend.py
+++ google-trend.py
@@ -2,4 +2,8 @@ from pytrends.request import TrendReq
 import matplotlib.pyplot as plt
 import os
 
-print()
+# 검색 keyword, 검색 기간 입력하기
+keyword = "Galaxy Fold"
+period = "today 3-m"
+
+# Google Trend 접속하기
