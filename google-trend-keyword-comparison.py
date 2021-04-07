0b8cb848c583f36269a714b76f5e0159616ea9d2
shawnbae <tngks0315@naver.com>
2021-04-08 08:02:47 +0900

    20210408 update

--- google-trend-keyword-comparison.py
+++ google-trend-keyword-comparison.py
@@ -2,4 +2,9 @@ from pytrends.request import TrendReq
 import matplotlib.pyplot as plt
 import os
 
+# 검색 keyword, 검색 기간 입력
+keyword1 = "apple iphone"
+keyword2 = "samsung galaxy"
+period = "today 5-y" # 검색 기간 5
 
+# 
