580a4c6b2027b6867e3208bf43bfacf1b9d6ce03
shawnbae <tngks0315@naver.com>
2021-04-07 15:28:37 +0900

    20210407 update

--- google-trend-region-comparison.py
+++ google-trend-region-comparison.py
@@ -17,12 +17,12 @@ print(trend_df.head())
 # 그래프 출력하기
 plt.style.use("ggplot")
 plt.figure(figsize= (14,10))
-trend_df.loc[:50, :][keyword].plot(kind='bar')
+trend_df.iloc[:50, :][keyword].plot(kind='bar')
 plt.title("Google Trends by Region", size = 15)
 plt.legend(labels = [keyword], loc="upper right")
 
 # 그래프 파일 저장하기
 cwd = os.getcwd()
 output_filepath = os.path.join(cwd, "output", "google_trend_by_region_%s.png" % keyword)
-plt.save(output_filepath, dpi=300)
+plt.savefig(output_filepath, dpi=300)
 plt.show()
