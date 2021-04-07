bdc8724b921f6270b7b572220acdc432513b11f7
shawnbae <tngks0315@naver.com>
2021-04-07 15:23:33 +0900

    20210407 update

--- google-trend-region-comparison.py
+++ google-trend-region-comparison.py
@@ -6,3 +6,12 @@ import os
 keyword = "WTO"
 period = "now 7-d" # 검색 기간: 최근 7일
 
+# Google Trend 접속
+trend_obj = TrendReq()
+trend_obj.build_payload(kw_list = [keyword], timeframe=period)
+
+# 지역별 검색 Trend 비교
+trend_df = trend_obj.interest_by_region().sort_values(by= keyword, ascending=False)
+print(trend_df.head())
+
+# 그래프 출력하기
