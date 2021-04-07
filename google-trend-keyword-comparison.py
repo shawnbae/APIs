263c9232c6e92b114b5389669b393596865d680a
shawnbae <tngks0315@naver.com>
2021-04-08 08:04:23 +0900

    20210408 update

--- google-trend-keyword-comparison.py
+++ google-trend-keyword-comparison.py
@@ -7,4 +7,10 @@ keyword1 = "apple iphone"
 keyword2 = "samsung galaxy"
 period = "today 5-y" # 검색 기간 5
 
-# 
+# Google Trend 접속 및 데이터 탑재
+trend_obj = TrendReq()
+trend_obj.build_payload(kw_list = [keyword1, keyword2], timeframe=period)
+trend_df = trend_obj.interest_over_time()
+
+# 그래
+
